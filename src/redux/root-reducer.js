331d9b781239aeb88ab9fbb66abf8014b82f21f1
ali <ali008009@yahoo.com>
2020-04-29 04:47:32 +0100

    added redux-persist to ensure our session will not lost after page referesh

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,21 @@
 import {combineReducers} from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userRducer from './user/user-reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 
-export default combineReducers({
-user:userRducer,
-cart:cartReducer
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist:['cart']
+}
 
-});
+const rootReducer =  combineReducers({
+    user:userRducer,
+    cart:cartReducer,
+    directory:directoryReducer
+    });
+
+
+export default persistReducer(persistConfig, rootReducer);
